cd42493de714326e9c1dbdfe4c2d847c7a5d6a3a
Maxkipkorir <77729472+Maxkipkorir@users.noreply.github.com>
2021-03-10 16:30:29 +0300

    Add files via upload

---
+++ max.py
@@ -0,0 +1,29 @@
+#Passing a list as an argument
+def my_function(food):
+    for s in food:
+        print(s)
+fruits=["apple","banana","grape"]
+my_function(fruits)
+
+def zetech(students):
+    for x in students:
+        print(x)
+men=["Kioko","Max","Taiga"]
+women=["Essy","Jane","Lidween"]
+my_function(men)
+my_function(women)
+
+
+def zetech(students):
+    for x in students:
+        print(x)
+students=["Kioko","Max","Taiga"]
+my_function(students)
+
+
+def area_of_a_circle(PI,radius):
+    x=PI*(radius * radius)
+    print("The area of the circle is",x)
+area_of_a_circle(3.142,13.5)
+
+
